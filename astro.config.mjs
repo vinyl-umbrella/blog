5c12ac456b68c64e84403ba8d75589b1ecfaeafb
vinyl-umbrella <61788251+vinyl-umbrella@users.noreply.github.com>
2023-01-31 21:30:55 +0900

    開発環境を整えた

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx'; // https://astro.build/config
+import sitemap from '@astrojs/sitemap'; // https://astro.build/config
+import tailwind from '@astrojs/tailwind'; // https://astro.build/config
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+    shikiConfig: {
+      theme: 'nord',
+    },
+  },
+});
