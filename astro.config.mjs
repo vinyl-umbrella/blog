829063d9f48155851a09cee6942631dadac7750e
vinyl-umbrella <61788251+vinyl-umbrella@users.noreply.github.com>
2023-02-05 16:08:39 +0900

    ogp imageの設定 @astro/imageの導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx'; // https://astro.build/config
+import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap'; // https://astro.build/config
 import tailwind from '@astrojs/tailwind'; // https://astro.build/config
+import image from '@astrojs/image'; // https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://manuke.dev',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    image({ serviceEntryPoint: '@astrojs/image/sharp' }),
+  ],
   markdown: {
     shikiConfig: {
       theme: 'monokai',
