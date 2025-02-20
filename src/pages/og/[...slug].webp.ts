0220710741e5a8a5c4a9fe67f27c1cca07a9569e
vinyl-umbrella <61788251+vinyl-umbrella@users.noreply.github.com>
2025-02-20 23:50:37 +0900

    astro upgrade

--- src/pages/og/[...slug].webp.ts
+++ src/pages/og/[...slug].webp.ts
@@ -4,13 +4,13 @@ import { getContents } from '../../utils/util';
 
 export async function getStaticPaths() {
   const posts = await getContents();
-  return posts.map((post) => ({ params: { slug: post.slug } }));
+  return posts.map((post) => ({ params: { slug: post.id } }));
 }
 
 export async function GET({ params }: APIContext) {
   const { slug } = params;
   if (!slug) return { status: 404 };
-  const posts = (await getContents()).find((post) => post.slug === slug);
+  const posts = (await getContents()).find((post) => post.id === slug);
   if (!posts) return { status: 404 };
 
   const body = await createOgImage(posts.data.title);
