ec8843b91f21192ff1ea029a729fe400c01b3146
vinyl-umbrella <61788251+vinyl-umbrella@users.noreply.github.com>
2026-05-03 21:07:26 +0900

    add katex

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeKatex from 'rehype-katex';
 import rehypeSlug from 'rehype-slug';
 import remarkBreaks from 'remark-breaks';
+import remarkMath from 'remark-math';
 import rehypeGithubPermalinkEmbed from './src/plugins/rehypeGithubPermalinkEmbed';
 import addCodeUtil from './src/plugins/remarkAddCodeUtil';
 import remarkGithubPermalinkEmbed from './src/plugins/remarkGithubPermalinkEmbed';
@@ -40,10 +42,12 @@ export default defineConfig({
       remarkGithubPermalinkEmbed,
       linkNewTab,
       remarkMermaidDetector,
+      remarkMath,
       remarkBreaks,
     ],
     rehypePlugins: [
       rehypeGithubPermalinkEmbed,
+      rehypeKatex,
       rehypeSlug,
       [
         rehypeAutolinkHeadings,
